c58b707f63b1ddbf1e14eddec473e01568d59eec
nan <renancaueassis2805@gmail.com>
2026-02-11 14:37:37 -0300

    Add files via upload

---
+++ js/teste/script.js
@@ -0,0 +1,32 @@
+// tipos de dados::
+
+//texto
+let nome = "Luis";
+let nome2 = 'pdid'
+
+console.log("Me chamo " + nome + " " + nome2)    ;
+console.log(nome2);
+
+//numero
+
+let num = "35";
+
+console.log(num);
+console.log("Minha idade é: " + num *2)
+
+//Booleanos
+let booleano = false;
+console.log("maior que 18 anois:" + booleano)
+
+//null e underfined
+let testeNull = null
+let testeUnderfined;
+
+console.log(testeNull);
+console.log(testeUnderfined);
+console.log(testeUnderfined * 2);
+
+console.log("6" + 4);
+console.log("6" - 4); 
+console.log("6" * 4); 
+console.log("6" / 4); 
