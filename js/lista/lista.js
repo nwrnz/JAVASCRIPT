83898b1fc2cdfdae0575aa7df35fe4a4ed8d1e3d
nan <renancaueassis2805@gmail.com>
2026-04-01 16:33:45 -0300

    Add files via upload

--- js/lista/lista.js
+++ js/lista/lista.js
@@ -10,3 +10,36 @@ let botaoNome = document.getElementById("btnMostrar")
     botaoNome.addEventListener("click", function(){
         alert (info.value);
     })
+
+let botao = document.getElementById("btnAdicionar")
+// 8 //
+let btnRemover = document.getElementById("btnRemover")
+    botao.addEventListener("click", function(){
+        let novoElemento = document.createElement("li");
+        novoElemento.textContent = "teste2";
+        document.getElementById("lista").appendChild(novoElemento);
+    })
+    btnRemover.addEventListener("dblclick", function(){
+        lista.removeChild(lista.lastElementChild)
+    })
+
+
+let cor =document.getElementById("btnCor")
+    cor.addEventListener("click", function(){
+        let caixinha = document.getElementById("caixa");
+        if (caixinha.style.backgroundColor == "blue"){
+         caixinha.style.backgroundColor = "red"   
+        }else{
+        caixinha.style.backgroundColor = "blue"}
+    })
+
+let clique = document.getElementById("btnClique")
+let contandor = 0
+    clique.addEventListener("click", function(){
+        contandor ++
+        console.log(contandor)
+    })
+
+
+
+
