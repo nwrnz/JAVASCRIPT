43a8d0fbdbd49933ab42ce2fb5bd0818f4ca53f4
nan <renancaueassis2805@gmail.com>
2026-05-13 16:38:24 -0300

    Add files via upload

--- Loja_Codigo_INFONET-master/script.js
+++ Loja_Codigo_INFONET-master/script.js
@@ -9,9 +9,9 @@ function renderizarProdutos(){
     const lista = document.getElementById("lista-produtos");
     lista.innerHTML ="";
 
-    produtosLoja.forEach(p => {
+    produtoLoja.forEach(p => {
         const div = document.createElement("div");
-        div.className ="Product";
+        div.className ="product";
 
         div.innerHTML = `
         <span>${p.nome} - R$ ${p.preco}<span>
@@ -20,48 +20,78 @@ function renderizarProdutos(){
 
         lista.appendChild(div);
     })
+}  
 
-    renderizarProdutos();
+renderizarProdutos();
 
-    function adicionarAoCarrinho(id){
-        const produto = produtoLoja.find(prod => prod.id == id);
+function adicionarAoCarrinho(id){
+    const produto = produtoLoja.find(prod => prod.id == id);
 
-        const existente = produtosCarrinho.find(prod=> prod.id ==id);
+    const existente = produtosCarrinho.find(prod=> prod.id ==id);
 
-        if (existente){
-            //aumentar a quantidade
-            existente.quantidade++;
-        }
-        else{
-            //criar o produto no carrinho
-            produtosCarrinho.push(
-                {
-                    //meu novo produto no carrinho
-                    id: produto.id,
-                    nome: produto.nome,
-                    preco: produto.preco,
-                    quantidade: 1
-
-
-                }
-            );
-        }
+    if (existente){
+        //aumentar a quantidade
+        existente.quantidade++;
     }
-    function renderizarCarrinho(){
-        const lista = document.getElementById("lista-carrinho");
-        const total = document.getElementById("total");
+    else{
+        //criar o produto no carrinho
+        produtosCarrinho.push(
+            {
+                //meu novo produto no carrinho
+                id: produto.id,
+                nome: produto.nome,
+                preco: produto.preco,
+                quantidade: 1
 
-        lista.innerHTML ="";
 
-        let somaTotal = 0;
-    
-        produtosCarrinho.forEach(produto => {
-            somaTotal = somaTotal + (produto.preco *produto.quantidade)
-            const div = document.createElement("div");
-            div.className ="cart-item"
-            div.innerHTML=`
-            
-            `
-        });
+            }
+        );
     }
+    renderizarCarrinho();   
+}
+
+function renderizarCarrinho(){
+    const lista = document.getElementById("lista-carrinho");
+    const total = document.getElementById("total");
+
+    lista.innerHTML ="";
+
+    let somaTotal = 0;
+
+    produtosCarrinho.forEach(produto => {
+        somaTotal = somaTotal + (produto.preco *produto.quantidade)
+        const div = document.createElement("div");
+        div.className ="cart-item"
+        
+        div.innerHTML=`
+        <span>${produto.nome} (x${produto.quantidade}) - R$ ${produto.preco * produto.quantidade}</span>
+        <div>
+        <button onclick="mudarQuantidade(${produto.id},1 )">
+        +
+        </button>
+        <button onclick="mudarQuantidade(${produto.id}, -1 )">
+               -
+        </button>
+        <button onclick="removerProduto(${produto.id})">
+        Remover
+        </button>
+        </div>
+        `;
+        lista.appendChild(div);
+    });
+    total.textContent = "Total: R$ " + somaTotal; 
+}
+function mudarQuantidade(id, valor){
+    produtosCarrinho = produtosCarrinho.map(produto =>{
+        if(id == produto.id){
+            return{
+                id: produto.id,
+                nome: produto.nome,
+                preco: produto.preco,
+                quantidade: produto.quantidade + valor
+            }
+        }
+        return produto;
+    });
+    renderizarCarrinho();
 }
